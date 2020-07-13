1914a5832b5f855515107916a3bfa01e5bdb7047
Jithu <jithu.george98@gmail.com>
2020-07-13 15:36:36 +0530

    Final file

---
+++ pro.js
@@ -0,0 +1,30 @@
+/* const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    checkInputs();
+ 
+
+});
+
+function checkInputs(){
+    const namevalue = name.value;
+    const emailvalue = email.value.trim();
+    const subjectvalue = subject.value;
+    const messagevalue = message.value;
+    
+
+}*/
+
+//function sendEmail() {
+	
+        //message => alert("mail sent successfully")
+          
+        //location.reload();
+	
+}
